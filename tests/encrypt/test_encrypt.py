c3e57e7c5f573464a5150579b15aec810ffbcafe
yuzomatoba <fabioymatoba@uol.com.br>
2023-06-01 16:12:58 -0300

    req. 01  challenge study schedule completed

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -2,4 +2,19 @@
 
 
 def test_encrypt_message():
-    pass
+    # Testando key e message incorretos:
+    try:
+        encrypted_message(231, 'Hey')
+    except exception as ex:
+        assert type(ex).__name__ == 'TypeError'
+
+    # Testando que key não é um índice válido de message:
+    assert encrypted_message(6, 'Hey') == 'yeH'
+    assert encrypted_message(12, 'Hey') == 'yeH'
+
+    # Testando se key é ímpar:
+    assert encrypted_message(3, 'Hey') == 'H_ey'
+    assert encrypted_message(5, 'Friend') == 'r_eiFn'
+
+    # Testando se key é par:
+    
