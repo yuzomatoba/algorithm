4cf423caa47d67bcf895632181d4595627ebb36a
yuzomatoba <fabioymatoba@uol.com.br>
2023-06-01 17:28:07 -0300

    fix. encrypted message test

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,39 +1,38 @@
 from challenges.challenge_encrypt_message import encrypt_message
 
-def encrypted_message(key, message):
-    if not isinstance(key, int) or not isinstance(message, str):
-        raise TypeError('key deve ser int e message deve ser do str')
 
-    if key < 0 or key >= len(message):
-        return message[::-1]
+# def encrypted_message(key, message):
+#     if not isinstance(key, int) or not isinstance(message, str):
+#         raise TypeError('key deve ser int e message deve ser do str')
 
-    inverted_words = [message[:key], message[key:]]
-    if key % 2 == 0:
-        inverted_words.reverse()
+#     if key < 0 or key >= len(message):
+#         return message[::-1]
 
-    encrypted_inverted_words = [word[::-1] for word in inverted_words]
-    encrypted_message = '_'.join(encrypted_inverted_words)
+#     inverted_words = [message[:key], message[key:]]
+#     if key % 2 == 0:
+#         inverted_words.reverse()
 
-    return encrypted_message
+#     encrypted_inverted_words = [word[::-1] for word in inverted_words]
+#     encrypted_message = '_'.join(encrypted_inverted_words)
 
-
+#     return encrypted_message
 def test_encrypt_message():
     # Testando key e message incorretos:
     try:
-        encrypted_message(231, 'Hey')
+        encrypt_message(231, 'Hey')
     except TypeError as ex:
         assert type(ex).__name__ == 'TypeError'
 
     # Testando que key não é um índice válido de message:
-    assert encrypted_message(6, 'Hey') == 'yeH'
-    assert encrypted_message(12, 'Hey') == 'yeH'
+    assert encrypt_message(6, 'Hey') == 'yeH'
+    assert encrypt_message(12, 'Hey') == 'yeH'
 
     # Testando se key é ímpar:
-    assert encrypted_message(1, 'Hey') == 'H_ey'
-    assert encrypted_message(3, 'Friend') == 'dne_irF'
+    assert encrypt_message(1, 'Hey') == 'H_ey'
+    assert encrypt_message(3, 'Friend') == 'dne_irF'
 
     # Testando se key é par:
-    assert encrypted_message(2, 'Hey') == 'He_y'
-    assert encrypted_message(4, 'Friend') == 'dn_eirF'
+    assert encrypt_message(2, 'Hey') == 'He_y'
+    assert encrypt_message(4, 'Friend') == 'dn_eirF'
 
     print('Testes realizados com sucesso')
