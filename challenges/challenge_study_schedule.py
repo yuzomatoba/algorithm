56b8f219cb16c7b4be5be8386e7bc88ea32b07fc
yuzomatoba <fabioymatoba@uol.com.br>
2023-06-01 15:56:49 -0300

    req. 01  challenge study schedule completed

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui.."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+    students_watching_class = 0
+    for starting_class, ending_class in permanence_period:
+        if not isinstance(starting_class, int) or not isinstance(ending_class, int):
+            return None
+        if starting_class <= target_time <= ending_class:
+            students_watching_class += 1
+            return students_watching_class
