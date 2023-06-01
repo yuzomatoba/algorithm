35e4af59ff3a540bd052a08671e2f80094f6d8e6
yuzomatoba <fabioymatoba@uol.com.br>
2023-06-01 17:54:02 -0300

    fix. encrypted message test

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,38 +1,18 @@
 from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
-# def encrypted_message(key, message):
-#     if not isinstance(key, int) or not isinstance(message, str):
-#         raise TypeError('key deve ser int e message deve ser do str')
-
-#     if key < 0 or key >= len(message):
-#         return message[::-1]
-
-#     inverted_words = [message[:key], message[key:]]
-#     if key % 2 == 0:
-#         inverted_words.reverse()
-
-#     encrypted_inverted_words = [word[::-1] for word in inverted_words]
-#     encrypted_message = '_'.join(encrypted_inverted_words)
-
-#     return encrypted_message
 def test_encrypt_message():
-    # Testando key e message incorretos:
-    try:
-        encrypt_message(231, 'Hey')
-    except TypeError as ex:
-        assert type(ex).__name__ == 'TypeError'
+    message = 'butterfly'
 
-    # Testando que key não é um índice válido de message:
-    assert encrypt_message(6, 'Hey') == 'yeH'
-    assert encrypt_message(12, 'Hey') == 'yeH'
+    with pytest.raises(TypeError, match='inválido o tipo de key'):
+        encrypt_message(message, None)
 
-    # Testando se key é ímpar:
-    assert encrypt_message(1, 'Hey') == 'H_ey'
-    assert encrypt_message(3, 'Friend') == 'dne_irF'
+    with pytest.raises(TypeError, match='inválido o tipo de message'):
+        encrypt_message(None, 0)
+   
+    assert encrypt_message(message, 0) == 'flybutter'
 
-    # Testando se key é par:
-    assert encrypt_message(2, 'Hey') == 'He_y'
-    assert encrypt_message(4, 'Friend') == 'dn_eirF'
+    assert encrypt_message(message, 1) == 'f_ylbtture'
 
-    print('Testes realizados com sucesso')
+    assert encrypt_message(message, 2) == 'btterul_yf'
