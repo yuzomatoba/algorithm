115abc40c97b1f77a8854777a2075c0fb2b1c052
yuzomatoba <fabioymatoba@uol.com.br>
2023-06-01 22:57:49 -0300

    challenge find the duplicate completed

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,9 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    for num in nums:
+        if type(num) != int or num < 0:
+            return False
+        
+        if nums.count(num) > 1:
+            return num
+    return False
+
