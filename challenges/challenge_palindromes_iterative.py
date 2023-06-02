5031b938ed695429f85ca802dbaccda72f674190
yuzomatoba <fabioymatoba@uol.com.br>
2023-06-02 17:15:50 -0300

    challenge palindromes iterative completed

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,9 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+   inverted_word = word[::-1]
+   if not word or word == '':
+      return False
+   if word == inverted_word:
+      return True
+   if word != inverted_word:
+      return False
+    
